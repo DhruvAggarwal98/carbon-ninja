0347afefcdb68277bbb3a5ef99837be35c924e28
Zach LeBlanc <zleblanc@redhat.com>
2021-05-09 18:17:51 -0600

    test endpoint works with IBM vision API

--- flask/api/predict/__init__.py
+++ flask/api/predict/__init__.py
@@ -1,9 +1,17 @@
+import os
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from PIL import Image
+from watson_developer_cloud import VisualRecognitionV3
 
 class Predict(Resource):
+  def __init__(self):
+    self.classifier = VisualRecognitionV3(
+      '2018-03-19',
+      iam_apikey=os.environ['IBM_VISION_API_KEY']
+    )
+
   def post(self):
-    file = request.files['image']
-    img = Image.open(file.stream)
-    return jsonify({'msg': 'success', 'size': [img.width, img.height]})
+    file_content = request.files['image']
+    classes = self.classifier.classify(file_content, threshold='0.6', classifier_ids='food')
+    return jsonify(classes.get_result())
