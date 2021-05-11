e38d2bd95ca87a70a8b3e16345cda3a90c805c87
jtorcass <jtorcass@redhat.com>
2021-05-11 09:16:00 -0400

    added sql query to api call

--- flask/api/foods/__init__.py
+++ flask/api/foods/__init__.py
@@ -15,4 +15,28 @@ class Foods(Resource):
   #  Used to populate app drop-down
   def get(self):
     print("getting all foods...")
+    
+    # Connect to MariaDB
+    # this function needs to be stored somewhere else but i couldnt get import to work
+    try:
+        conn = mariadb.connect(
+                user="admin",
+                password="pass",
+                host="mariadb",
+                port=3306,
+                database="kaggledb")
+
+    # Throw error msg and exit
+    except mariadb.Error as e:
+        print(f"Error while connection to Mariadb: {e}")
+        sys.exit(1)
+    
+    # Get cursor and query MariaDB
+    cur = conn.cursor()
+    cur.execute("SELECT food_product FROM food_production;")
+
+    # Print results
+    for food_product in cur:
+        print(f"Food: {food_product}")
+
     return "Success"
