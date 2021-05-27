0320967fd747a0b970a3dab2c71ab27e74dc6441
jtorcass <jtorcass@redhat.com>
2021-05-27 15:34:57 -0400

    update file path

--- oc/run.sh
+++ oc/run.sh
@@ -2,6 +2,6 @@
 local thisdir
 local init_data_file
 thisdir=$(dirname ${BASH_SOURCE[0]})
-init_data_file=$(readlink -f ${thisdir}/sql-init.sql)
+init_data_file=$(readlink -f /opt/app-root/src/mysql-init/sql_init.sql)
 log_info "Initializing the arbitrary database from file ${init_data_file}..."
 mysql $mysql_flags ${MYSQL_DATABASE} < ${init_data_file}
