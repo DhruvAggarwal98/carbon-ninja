1837432ca1d29334c3daf1ea932fa636f5be30d9
jtorcass <jtorcass@redhat.com>
2021-05-27 16:18:20 -0400

    change branch back to dev

--- oc/create_app.sh
+++ oc/create_app.sh
@@ -4,7 +4,7 @@ oc new-project "${PROJECT_NAME}"
 oc create secret generic db-secret --from-literal=user=admin --from-literal=pass=pw
 oc create -f template.yml
 oc new-app --template=call-for-code-template \
-	-p BRANCH=feature/initialize-db \
+	-p BRANCH=dev \
 	-p NAME="${PROJECT_NAME}" \
 	-p MYSQL_DATABASE=db 
 
