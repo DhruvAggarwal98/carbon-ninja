1e7aedb562e857ee926949240b63bb6621c3c1bb
jtorcass <jtorcass@redhat.com>
2021-05-27 15:26:38 -0400

    update run.sh with local vars

--- oc/run.sh
+++ oc/run.sh
@@ -1,1 +1,7 @@
-mysql $mysql_flags < /opt/app-root/src/mysql-init/sql_init.sql
+#mysql $mysql_flags < /opt/app-root/src/mysql-init/sql_init.sql
+local thisdir
+local init_data_file
+thisdir=$(dirname ${BASH_SOURCE[0]})
+init_data_file=$(readlink -f ${thisdir}/sql-init.sql)
+log_info "Initializing the arbitrary database from file ${init_data_file}..."
+mysql $mysql_flags ${MYSQL_DATABASE} < ${init_data_file}
