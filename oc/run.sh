7d55e6cb3634b6e7c86b58afee6f00fbcb46b073
jtorcass <jtorcass@redhat.com>
2021-05-27 15:43:04 -0400

    polish up files

--- oc/run.sh
+++ oc/run.sh
@@ -1,7 +1,5 @@
-#mysql $mysql_flags < /opt/app-root/src/mysql-init/sql_init.sql
-local thisdir
 local init_data_file
-thisdir=$(dirname ${BASH_SOURCE[0]})
 init_data_file=$(readlink -f /opt/app-root/src/mysql-init/sql_init.sql)
-log_info "Initializing the arbitrary database from file ${init_data_file}..."
+log_info "Initializing database from file ${init_data_file}..."
 mysql $mysql_flags ${MYSQL_DATABASE} < ${init_data_file}
+log_info "Database initialized."
