feba93c0a37fe89d812fc3fb23f4ae8303acaaf4
dkhater <dkhater@redhat.com>
2021-06-02 22:16:12 -0500

    added functionality to mapping after meeting, removed generic foods from csv

--- flask/services/mapping.py
+++ flask/services/mapping.py
@@ -11,10 +11,14 @@ class FuzzySearchService():
         #pull from database
         #get request to FOODS_API_BASE_URL=http://call-for-code-route-call-for-code.apps.shared-na46.openshift.opentlc.com
         self.csv = '/Users/daliakhater/callforcode/call-for-code/flask/files/Food_Production_Cleanup.csv'
-        self.classifier = VisualRecognitionV3('2018-03-19',iam_apikey=os.environ['IBM_VISION_API_KEY'])
+        self.classifier = VisualRecognitionV3('2018-03-19', iam_apikey=os.environ['IBM_VISION_API_KEY'])
         self.df = pd.read_csv(self.csv, index_col=False)
         self.prediction_foods = []
-        self.fuzzy_prediction_score = []
+        self.fuzzy_predictions = []
+        self.predictions = []
+        self.carbon_emissions = []
+        self.total_emissions = 0
+        self.fuzzy_threshold = 75
 
     def read_csv(self):
         food_choice_df = self.df.iloc[:,0]
@@ -30,32 +34,33 @@ class FuzzySearchService():
     def find_watson_prediction_choices(self, classes):
         for entry in classes['images'][0]['classifiers'][0]['classes']:
             self.prediction_foods.append(entry['class'])
-
-        print(self.prediction_foods)
         return self.prediction_foods
 
     def find_prediction(self, food_list, prediction_foods):
         for currpred in prediction_foods:
             (food_prediction, score) = process.extractOne(currpred, food_list)
-            if score > 70:
-                self.fuzzy_prediction_score.append(process.extractOne(currpred, food_list))
-        self.fuzzy_prediction_score = [item[::-1] for item in self.fuzzy_prediction_score]
-        self.fuzzy_prediction_score.sort(key=None, reverse=True)
-        (score, prediction) = self.fuzzy_prediction_score[0]
-        if not self.fuzzy_prediction_score:
+            if (score > self.fuzzy_threshold) and (food_prediction not in self.fuzzy_predictions):
+                self.fuzzy_predictions.append(food_prediction)
+        if not self.fuzzy_predictions:
             print('no results found')
             return False
-        return prediction
+        return self.fuzzy_predictions
 
-    def find_carbon_emission(self, prediction):
-        carbon_emission = self.df[self.df['Food product'] == prediction]
-        return carbon_emission
+    def find_carbon_emission(self, fuzzy_predictions):
+        for prediction in fuzzy_predictions:
+            prediction_row = self.df[self.df['Food product'] == prediction]
+            carbon_emission = prediction_row.Total_emissions.item()
+            self.carbon_emissions.append([prediction, carbon_emission])
+            self.total_emissions = self.total_emissions + carbon_emission
+        return self.carbon_emissions, self.total_emissions
+
 
 
 search = FuzzySearchService(image='/Users/daliakhater/callforcode/call-for-code/flask/files/fruit.jpg')
 food_list = search.read_csv()
 classes = search.pull_image_data()
 prediction_foods = search.find_watson_prediction_choices(classes)
-prediction = search.find_prediction(food_list, prediction_foods)
-carbon_emission = search.find_carbon_emission(prediction)
-print(carbon_emission)
+fuzzy_predictions = search.find_prediction(food_list, prediction_foods)
+(carbon_emissions, total_emissions) = search.find_carbon_emission(fuzzy_predictions)
+print(carbon_emissions)
+print(total_emissions)
