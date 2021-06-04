216ec70022d11e702e52dbc9372b4b1c5f85a57c
Dhruv Aggarwal <aggarw45@gmail.com>
2021-06-04 10:12:54 -0400

    deleted react folder

--- react/src/modules/components/ComponentsViewContainer.js
+++
@@ -1,7 +0,0 @@
-import { compose, withState } from 'recompose';
-
-import ComponentsScreen from './ComponentsView';
-
-export default compose(
-  withState('radioGroupsState', 'setRadioGroupsState', [0, 0]),
-)(ComponentsScreen);
--- react/src/modules/pages/PagesViewContainer.js
+++
@@ -1,6 +0,0 @@
-// @flow
-import { compose } from 'recompose';
-
-import PagesView from './PagesView';
-
-export default compose()(PagesView);
