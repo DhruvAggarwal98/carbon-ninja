5d17a9191794ae394905a44c4e35029acf932e76
Zach LeBlanc <zleblanc@alumni.nd.edu>
2021-06-05 13:25:36 -0500

    delete node_modules

--- carbon-ninja/node_modules/expo-camera/plugin/build/withCamera.d.ts
+++
@@ -1,7 +0,0 @@
-import { ConfigPlugin } from '@expo/config-plugins';
-export declare function setGradleMaven(buildGradle: string): string;
-declare const _default: ConfigPlugin<void | {
-    cameraPermission?: string | undefined;
-    microphonePermission?: string | undefined;
-}>;
-export default _default;
