91f446f941e5661c1873a74dc6b30735da8e22c7
jtorcass <jtorcass@redhat.com>
2021-06-09 14:08:43 -0400

    revert to dev branch

--- oc/create_app.sh
+++ oc/create_app.sh
@@ -1,11 +1,11 @@
 # Script to run the template with all parameters
-PROJECT_NAME="call-for-code-jt"
+PROJECT_NAME="call-for-code"
 oc delete project "${PROJECT_NAME}"
 oc new-project "${PROJECT_NAME}"
 oc create secret generic db-secret --from-literal=user=admin --from-literal=pass=pw
 oc create -f template.yml
 oc new-app --template=call-for-code-template \
-	-p BRANCH=get-emissions-from-food \
+	-p BRANCH=dev \
 	-p NAME="${PROJECT_NAME}" \
 	-p MYSQL_DATABASE=db 
 
