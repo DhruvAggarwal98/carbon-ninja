c608a10c82e5099c2d2de89b02e2acf63e4d80f3
jtorcass <jtorcass@redhat.com>
2021-06-10 21:57:39 -0400

    swapped read from csv to loading food_list from db

--- flask/services/mapping.py
+++ flask/services/mapping.py
@@ -1,6 +1,7 @@
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 from watson_developer_cloud import VisualRecognitionV3
+from mariadb import MariaDBService
 import csv
 import os
 import pandas as pd
@@ -10,9 +11,11 @@ class FuzzySearchService():
         self.image = image
         #pull from database
         #get request to FOODS_API_BASE_URL=http://call-for-code-route-call-for-code.apps.shared-na46.openshift.opentlc.com
-        self.csv = '/Users/daliakhater/callforcode/call-for-code/flask/files/Food_Production_Cleanup.csv'
+#        self.csv = '/Users/daliakhater/callforcode/call-for-code/flask/files/Food_Production_Cleanup.csv'
         self.classifier = VisualRecognitionV3('2018-03-19', iam_apikey=os.environ['IBM_VISION_API_KEY'])
-        self.df = pd.read_csv(self.csv, index_col=False)
+       # self.df = pd.read_csv(self.csv, index_col=False)
+        self.db = MariaDBService() # connect to db
+        self.food_list = self.db.get_all_foods() # get food list
         self.prediction_foods,  self.fuzzy_predictions, self.predictions, self.carbon_emissions = [], [], [], []
         self.total_emissions = 0
         self.fuzzy_threshold = 75
@@ -55,7 +58,7 @@ class FuzzySearchService():
 
 #fix so that it pulls from image captured from image captured from camera
 search = FuzzySearchService(image='/Users/daliakhater/callforcode/call-for-code/flask/files/food/veggie-tray_01.jpeg')
-food_list = search.read_csv()
+# food_list = search.read_csv()
 classes = search.pull_image_data()
 prediction_foods = search.find_watson_prediction_choices(classes)
 fuzzy_predictions = search.find_prediction(food_list, prediction_foods)
