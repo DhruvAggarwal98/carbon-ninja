8eeddaedd2c78f536a629eb2b406df04a92c3afa
jtorcass <jtorcass@redhat.com>
2021-06-15 22:55:05 -0400

    updated predict endpoint to return emissions with predicted foods

--- flask/api/predict/__init__.py
+++ flask/api/predict/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from watson_developer_cloud import VisualRecognitionV3
 from services import FuzzySearchService
+from services import MariaDBService
 
 class Predict(Resource):
   def __init__(self):
@@ -12,9 +13,11 @@ class Predict(Resource):
       url=os.environ['IBM_VISION_URL']
     )
     self.search = FuzzySearchService()
+    self.db = MariaDBService()
 
   def post(self):
     file_content = request.files['image']
     classes = self.classifier.classify(file_content, threshold='0.6', classifier_ids='food')
     resp_json = classes.get_result()
-    return jsonify(self.search.find_watson_prediction_choices(resp_json))
+    fuzzy_preds = self.search.get_fuzzy_prediction(resp_json) # list of predicted foods
+    return self.db.get_food_emissions(fuzzy_preds)
