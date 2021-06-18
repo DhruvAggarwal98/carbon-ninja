63eab85d666fd3e0777a8ee419e094cd9a5ff858
jtorcass <jtorcass@redhat.com>
2021-06-17 20:48:46 -0400

    update dockerfile and sql scripts to add tables

--- oc/run.sh
+++ oc/run.sh
@@ -1,5 +1,7 @@
-local init_data_file
-init_data_file=$(readlink -f /opt/app-root/src/mysql-init/sql_init.sql)
-log_info "Initializing database from file ${init_data_file}..."
-mysql $mysql_flags ${MYSQL_DATABASE} < ${init_data_file}
+local init_foods_file, init_users_file
+init_foods_file=$(readlink -f /opt/app-root/src/mysql-init/sql_init.sql)
+init_users_file=$(readlink -f /opt/app-root/src/mysql-init/users_init.sql)
+log_info "Initializing database from files ${init_foods_file} and ${init_users_file}..."
+mysql $mysql_flags ${MYSQL_DATABASE} < ${init_foods_file}
+mysql $mysql_flags ${MYSQL_DATABASE} < ${init_users_file}
 log_info "Database initialized."
